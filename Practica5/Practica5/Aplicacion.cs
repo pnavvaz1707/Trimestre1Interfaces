ee99d18c306dcbaebd3be4729318ad6ce96b579f
pablo <pablonnavarrovazquez@gmail.com>
2022-11-16 14:06:18 +0100

    Falta método subrayado

--- Practica5/Practica5/Aplicacion.cs
+++ Practica5/Practica5/Aplicacion.cs
@@ -29,8 +29,8 @@ namespace Practica5
 
         public static void Main(string[] args)
         {
-            int opcion;
             Grupo g = crearGrupo();
+            int opcion;
             do
             {
                 opcion = menu(g);
@@ -55,7 +55,94 @@ namespace Practica5
                         break;
 
                     case 4:
+                        List<string> encabezado = new List<string>();
 
+                        encabezado.Add("MATRÍCULA");
+                        Console.Write(encabezado[0]);
+                        Console.SetCursorPosition(Console.CursorLeft + 2, Console.CursorTop);
+
+                        encabezado.Add("NOMBRE");
+                        Console.Write(encabezado[1]);
+                        Console.SetCursorPosition(Console.CursorLeft + 22, Console.CursorTop);
+                        for (int i = 0; i < g.CodigosAsignaturas.Length; i++)
+                        {
+                            encabezado.Add(g.CodigosAsignaturas[i]);
+                            Console.Write(g.CodigosAsignaturas[i]);
+                            Console.SetCursorPosition(Console.CursorLeft + 2, Console.CursorTop);
+                        }
+                        encabezado.Add("MEDIA");
+                        Console.Write(encabezado[g.CodigosAsignaturas.Length + 2]);
+                        Console.SetCursorPosition(Console.CursorLeft + 2, Console.CursorTop);
+                        encabezado.Add("Nº SUS");
+                        Console.WriteLine(encabezado[g.CodigosAsignaturas.Length + 3]);
+
+                        for (int i = 0; i < encabezado[0].Length; i++)
+                        {
+                            Console.Write("-");
+                        }
+                        Console.SetCursorPosition(Console.CursorLeft + 2, Console.CursorTop);
+
+                        for (int i = 0; i < encabezado[1].Length; i++)
+                        {
+                            Console.Write("-");
+                        }
+                        Console.SetCursorPosition(Console.CursorLeft + 22, Console.CursorTop);
+
+                        for (int i = 0; i < g.CodigosAsignaturas.Length; i++)
+                        {
+                            Console.Write("---");
+                            Console.SetCursorPosition(Console.CursorLeft + 2, Console.CursorTop);
+                        }
+
+                        for (int i = 0; i < encabezado[g.CodigosAsignaturas.Length + 2].Length; i++)
+                        {
+                            Console.Write("-");
+                        }
+                        Console.SetCursorPosition(Console.CursorLeft + 2, Console.CursorTop);
+                        for (int i = 0; i < encabezado[g.CodigosAsignaturas.Length + 3].Length; i++)
+                        {
+                            Console.Write("-");
+                        }
+
+                        int numSuspensos;
+                        int numSuspensos0 = 0;
+                        int numSuspensos1 = 0;
+                        int numSuspensos2 = 0;
+
+                        g.Alumnos.Sort();
+                        for (int i = 0; i < g.Alumnos.Count; i++)
+                        {
+
+
+
+                            Console.WriteLine("\nNota media: " + g.Alumnos[i].mediaAlumno());
+
+                            numSuspensos = g.Alumnos[i].numSuspensosAlumno();
+                            Console.WriteLine("Número de suspensos: " + numSuspensos);
+                            switch (numSuspensos)
+                            {
+                                case 0:
+                                    numSuspensos0++;
+                                    break;
+                                case 1:
+                                    numSuspensos1++;
+                                    break;
+                                case 2:
+                                    numSuspensos2++;
+                                    break;
+
+                            }
+                        }
+                        Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop);
+                        for (int i = 0; i < g.Alumnos[0].Notas.Length; i++)
+                        {
+                            Console.SetCursorPosition(Console.CursorLeft + 5, Console.CursorTop);
+                            Console.WriteLine("Media del grupo (asignatura " + g.CodigosAsignaturas[i] + "): " + g.mediaAsignatura(i));
+                        }
+                        Console.WriteLine("Alumnos con 0 asignaturas suspensas: " + numSuspensos0);
+                        Console.WriteLine("Alumnos con 1 asignaturas suspensa: " + numSuspensos1);
+                        Console.WriteLine("Alumnos con 2 asignaturas suspensas: " + numSuspensos2);
+                        Console.WriteLine("Alumnos con 3 o más asignaturas suspensas: " + (g.Alumnos.Count - numSuspensos0 - numSuspensos1 - numSuspensos2));
                         Auxiliar.pulsarParaContinuar();
                         break;
                 }
@@ -66,7 +153,9 @@ namespace Practica5
         {
             string nombre = Auxiliar.leerCadena("Indica el nombre del grupo: ");
             int numAsignaturas = Auxiliar.solicitarEnteroEnUnRango(5, 9, "Indica el número de asignaturas: ");
+
             string[] codigosAsignaturas = new string[numAsignaturas];
+
             Console.WriteLine("Escriba los códigos de las asignaturas (3 caracteres)");
             for (int i = 0; i < codigosAsignaturas.Length; i++)
             {
--- Practica5/Practica5/Grupo.cs
+++ Practica5/Practica5/Grupo.cs
@@ -15,6 +15,7 @@ namespace Practica5
 
         public string Nombre { get { return nombre; } }
         public int NumAsignaturas { get { return numAsignaturas; } }
+        public string[] CodigosAsignaturas { get { return codigosAsignaturas; } }
         public List<Alumno> Alumnos { get { return alumnos; } }
 
         public Grupo(string nombre, int numAsignaturas, string[] codigosAsignaturas)
@@ -69,56 +70,11 @@ namespace Practica5
         public float mediaAsignatura(int posicionAsignatura)
         {
             float total = 0;
-            Console.WriteLine("Tamaño alumnos --> " + alumnos.Count);
             for (int i = 0; i < alumnos.Count; i++)
             {
                 total += alumnos[i].Notas[posicionAsignatura];
             }
             return (total / alumnos.Count);
         }
-
-        public void actaGrupo()
-        {
-            int numSuspensos = 0;
-            int numSuspensos0 = 0;
-            int numSuspensos1 = 0;
-            int numSuspensos2 = 0;
-
-            alumnos.Sort();
-            for (int i = 0; i < alumnos.Count; i++)
-            {
-                alumnos[i].imprimeAlumno();
-                Console.Write("Notas: " + alumnos[i].Notas.ToString());
-                /*for (int j = 0; j < alumnos[i].Notas.Length; j++)
-                {
-                    Console.Write(alumnos[i].Notas[j]);
-                }*/
-                Console.WriteLine("\nNota media: " + alumnos[i].mediaAlumno());
-                numSuspensos = alumnos[i].numSuspensosAlumno();
-                Console.WriteLine("Número de suspensos: " + numSuspensos);
-                switch (numSuspensos)
-                {
-                    case 0:
-                        numSuspensos0++;
-                        break;
-                    case 1:
-                        numSuspensos1++;
-                        break;
-                    case 2:
-                        numSuspensos2++;
-                        break;
-
-                }
-            }
-            for (int i = 0; i < alumnos[0].Notas.Length; i++)
-            {
-                Console.WriteLine("Media del grupo (asignatura " + i + "): " + mediaAsignatura(i));
-            }
-            Console.WriteLine("Alumnos con 0 asignaturas suspensas: " + numSuspensos0);
-            Console.WriteLine("Alumnos con 1 asignaturas suspensa: " + numSuspensos1);
-            Console.WriteLine("Alumnos con 2 asignaturas suspensas: " + numSuspensos2);
-            Console.WriteLine("Alumnos con 3 o más asignaturas suspensas: " + (alumnos.Count - numSuspensos0 - numSuspensos1 - numSuspensos2));
-        }
-
     }
 }
