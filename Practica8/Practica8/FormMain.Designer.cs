d9ea4e3b974a4f7b58b8b48a36c662d8ea98ac45
Pnavvaz <120363753+Pnavvaz@users.noreply.github.com>
2023-01-19 18:27:51 +0100

    fal

--- Practica8/Practica8/FormMain.Designer.cs
+++ Practica8/Practica8/FormMain.Designer.cs
@@ -41,20 +41,26 @@ namespace Practica8
             this.btnNuevo = new System.Windows.Forms.Button();
             this.lblTitulo = new System.Windows.Forms.Label();
             this.dtgvGrupos = new System.Windows.Forms.DataGridView();
-            this.txtNombreEditarGrupo = new System.Windows.Forms.TextBox();
-            this.lblNombreEditarGrupo = new System.Windows.Forms.Label();
-            this.btnEditarGrupo = new System.Windows.Forms.Button();
             this.dtgvAlumnos = new System.Windows.Forms.DataGridView();
             this.btnAgregarAlumno = new System.Windows.Forms.Button();
             this.btnBorrarAlumno = new System.Windows.Forms.Button();
-            this.lstAsignaturasEditarGrupo = new System.Windows.Forms.CheckedListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuArchivo = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuNuevoBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuAbrirBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.menuSalirBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuAyuda = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuAcercaDeBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dtgvGrupos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dtgvAlumnos)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnImportar
             // 
-            this.btnImportar.Location = new System.Drawing.Point(399, 306);
+            this.btnImportar.Location = new System.Drawing.Point(399, 275);
             this.btnImportar.Name = "btnImportar";
             this.btnImportar.Size = new System.Drawing.Size(105, 49);
             this.btnImportar.TabIndex = 1;
@@ -64,7 +70,7 @@ namespace Practica8
             // 
             // btnNuevo
             // 
-            this.btnNuevo.Location = new System.Drawing.Point(510, 306);
+            this.btnNuevo.Location = new System.Drawing.Point(510, 275);
             this.btnNuevo.Name = "btnNuevo";
             this.btnNuevo.Size = new System.Drawing.Size(105, 49);
             this.btnNuevo.TabIndex = 3;
@@ -76,7 +82,7 @@ namespace Practica8
             // 
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lblTitulo.Location = new System.Drawing.Point(226, 3);
+            this.lblTitulo.Location = new System.Drawing.Point(226, 23);
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.Size = new System.Drawing.Size(236, 36);
             this.lblTitulo.TabIndex = 4;
@@ -86,51 +92,26 @@ namespace Practica8
             // 
             this.dtgvGrupos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dtgvGrupos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgvGrupos.Location = new System.Drawing.Point(45, 45);
+            this.dtgvGrupos.Location = new System.Drawing.Point(45, 71);
             this.dtgvGrupos.Name = "dtgvGrupos";
             this.dtgvGrupos.RowTemplate.Height = 25;
-            this.dtgvGrupos.Size = new System.Drawing.Size(570, 255);
+            this.dtgvGrupos.Size = new System.Drawing.Size(570, 198);
             this.dtgvGrupos.TabIndex = 5;
             this.dtgvGrupos.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgvGrupos_RowEnter);
-            // 
-            // txtNombreEditarGrupo
-            // 
-            this.txtNombreEditarGrupo.Location = new System.Drawing.Point(101, 401);
-            this.txtNombreEditarGrupo.Name = "txtNombreEditarGrupo";
-            this.txtNombreEditarGrupo.Size = new System.Drawing.Size(100, 23);
-            this.txtNombreEditarGrupo.TabIndex = 20;
-            // 
-            // lblNombreEditarGrupo
-            // 
-            this.lblNombreEditarGrupo.AutoSize = true;
-            this.lblNombreEditarGrupo.Location = new System.Drawing.Point(44, 404);
-            this.lblNombreEditarGrupo.Name = "lblNombreEditarGrupo";
-            this.lblNombreEditarGrupo.Size = new System.Drawing.Size(51, 15);
-            this.lblNombreEditarGrupo.TabIndex = 27;
-            this.lblNombreEditarGrupo.Text = "Nombre";
-            // 
-            // btnEditarGrupo
-            // 
-            this.btnEditarGrupo.Location = new System.Drawing.Point(510, 688);
-            this.btnEditarGrupo.Name = "btnEditarGrupo";
-            this.btnEditarGrupo.Size = new System.Drawing.Size(105, 49);
-            this.btnEditarGrupo.TabIndex = 28;
-            this.btnEditarGrupo.Text = "Guardar";
-            this.btnEditarGrupo.UseVisualStyleBackColor = true;
             // 
             // dtgvAlumnos
             // 
             this.dtgvAlumnos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dtgvAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgvAlumnos.Location = new System.Drawing.Point(226, 404);
+            this.dtgvAlumnos.Location = new System.Drawing.Point(45, 414);
             this.dtgvAlumnos.Name = "dtgvAlumnos";
             this.dtgvAlumnos.RowTemplate.Height = 25;
-            this.dtgvAlumnos.Size = new System.Drawing.Size(389, 198);
+            this.dtgvAlumnos.Size = new System.Drawing.Size(570, 240);
             this.dtgvAlumnos.TabIndex = 29;
             // 
             // btnAgregarAlumno
             // 
-            this.btnAgregarAlumno.Location = new System.Drawing.Point(510, 608);
+            this.btnAgregarAlumno.Location = new System.Drawing.Point(510, 660);
             this.btnAgregarAlumno.Name = "btnAgregarAlumno";
             this.btnAgregarAlumno.Size = new System.Drawing.Size(105, 49);
             this.btnAgregarAlumno.TabIndex = 30;
@@ -140,7 +121,7 @@ namespace Practica8
             // 
             // btnBorrarAlumno
             // 
-            this.btnBorrarAlumno.Location = new System.Drawing.Point(399, 608);
+            this.btnBorrarAlumno.Location = new System.Drawing.Point(399, 660);
             this.btnBorrarAlumno.Name = "btnBorrarAlumno";
             this.btnBorrarAlumno.Size = new System.Drawing.Size(105, 49);
             this.btnBorrarAlumno.TabIndex = 31;
@@ -148,20 +129,80 @@ namespace Practica8
             this.btnBorrarAlumno.UseVisualStyleBackColor = true;
             this.btnBorrarAlumno.Click += new System.EventHandler(this.btnBorrarAlumno_Click);
             // 
-            // lstAsignaturasEditarGrupo
+            // menuStrip1
             // 
-            this.lstAsignaturasEditarGrupo.FormattingEnabled = true;
-            this.lstAsignaturasEditarGrupo.Items.AddRange(new object[] {
-            "LEMA",
-            "PRO",
-            "ENDE",
-            "FOL",
-            "SIGE",
-            "DI"});
-            this.lstAsignaturasEditarGrupo.Location = new System.Drawing.Point(45, 456);
-            this.lstAsignaturasEditarGrupo.Name = "lstAsignaturasEditarGrupo";
-            this.lstAsignaturasEditarGrupo.Size = new System.Drawing.Size(120, 94);
-            this.lstAsignaturasEditarGrupo.TabIndex = 32;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuArchivo,
+            this.menuAyuda});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(663, 24);
+            this.menuStrip1.TabIndex = 32;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuArchivo
+            // 
+            this.menuArchivo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuNuevoBtn,
+            this.menuAbrirBtn,
+            this.toolStripSeparator,
+            this.menuSalirBtn});
+            this.menuArchivo.Name = "menuArchivo";
+            this.menuArchivo.Size = new System.Drawing.Size(60, 20);
+            this.menuArchivo.Text = "&Archivo";
+            // 
+            // menuNuevoBtn
+            // 
+            this.menuNuevoBtn.Image = ((System.Drawing.Image)(resources.GetObject("menuNuevoBtn.Image")));
+            this.menuNuevoBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuNuevoBtn.Name = "menuNuevoBtn";
+            this.menuNuevoBtn.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.menuNuevoBtn.Size = new System.Drawing.Size(180, 22);
+            this.menuNuevoBtn.Text = "&Nuevo";
+            // 
+            // menuAbrirBtn
+            // 
+            this.menuAbrirBtn.Image = ((System.Drawing.Image)(resources.GetObject("menuAbrirBtn.Image")));
+            this.menuAbrirBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuAbrirBtn.Name = "menuAbrirBtn";
+            this.menuAbrirBtn.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.menuAbrirBtn.Size = new System.Drawing.Size(180, 22);
+            this.menuAbrirBtn.Text = "&Abrir";
+            // 
+            // toolStripSeparator
+            // 
+            this.toolStripSeparator.Name = "toolStripSeparator";
+            this.toolStripSeparator.Size = new System.Drawing.Size(177, 6);
+            // 
+            // menuSalirBtn
+            // 
+            this.menuSalirBtn.Name = "menuSalirBtn";
+            this.menuSalirBtn.Size = new System.Drawing.Size(180, 22);
+            this.menuSalirBtn.Text = "&Salir";
+            // 
+            // menuAyuda
+            // 
+            this.menuAyuda.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuAcercaDeBtn});
+            this.menuAyuda.Name = "menuAyuda";
+            this.menuAyuda.Size = new System.Drawing.Size(53, 20);
+            this.menuAyuda.Text = "&Ayuda";
+            // 
+            // menuAcercaDeBtn
+            // 
+            this.menuAcercaDeBtn.Name = "menuAcercaDeBtn";
+            this.menuAcercaDeBtn.Size = new System.Drawing.Size(180, 22);
+            this.menuAcercaDeBtn.Text = "&Acerca de...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(216, 375);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 36);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "Lista de alumnos";
             // 
             // FormMain
             // 
@@ -169,22 +210,23 @@ namespace Practica8
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(663, 749);
-            this.Controls.Add(this.lstAsignaturasEditarGrupo);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnBorrarAlumno);
             this.Controls.Add(this.btnAgregarAlumno);
             this.Controls.Add(this.dtgvAlumnos);
-            this.Controls.Add(this.txtNombreEditarGrupo);
-            this.Controls.Add(this.lblNombreEditarGrupo);
-            this.Controls.Add(this.btnEditarGrupo);
             this.Controls.Add(this.dtgvGrupos);
             this.Controls.Add(this.lblTitulo);
             this.Controls.Add(this.btnNuevo);
             this.Controls.Add(this.btnImportar);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "FormMain";
             this.Text = "Gestión de grupos";
             ((System.ComponentModel.ISupportInitialize)(this.dtgvGrupos)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dtgvAlumnos)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -195,12 +237,17 @@ namespace Practica8
         private Button btnNuevo;
         private Label lblTitulo;
         private DataGridView dtgvGrupos;
-        private TextBox txtNombreEditarGrupo;
-        private Label lblNombreEditarGrupo;
-        private Button btnEditarGrupo;
         private DataGridView dtgvAlumnos;
         private Button btnAgregarAlumno;
         private Button btnBorrarAlumno;
-        private CheckedListBox lstAsignaturasEditarGrupo;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem menuArchivo;
+        private ToolStripMenuItem menuNuevoBtn;
+        private ToolStripMenuItem menuAbrirBtn;
+        private ToolStripSeparator toolStripSeparator;
+        private ToolStripMenuItem menuSalirBtn;
+        private ToolStripMenuItem menuAyuda;
+        private ToolStripMenuItem menuAcercaDeBtn;
+        private Label label1;
     }
 }
