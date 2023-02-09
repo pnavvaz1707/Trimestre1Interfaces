df118408fd2d939e33ed528ed2387de96805c95b
Pnavvaz <120363753+Pnavvaz@users.noreply.github.com>
2023-02-09 14:08:57 +0100

    Falta enviar fichero

---
+++ Practica10/Practica10/FormChat.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Practica10
+{
+    partial class FormChat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormChat));
+            this.menuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercadeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rtboxMensajesRecibidos = new System.Windows.Forms.RichTextBox();
+            this.tboxMensajeAEnviar = new System.Windows.Forms.TextBox();
+            this.btnEnviarFichero = new System.Windows.Forms.Button();
+            this.lblMensajesRecibidos = new System.Windows.Forms.Label();
+            this.btnEnviarMensaje = new System.Windows.Forms.Button();
+            this.menuPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuPrincipal
+            // 
+            this.menuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.acercadeToolStripMenuItem});
+            this.menuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.menuPrincipal.Name = "menuPrincipal";
+            this.menuPrincipal.Size = new System.Drawing.Size(446, 24);
+            this.menuPrincipal.TabIndex = 1;
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "&Archivo";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("nuevoToolStripMenuItem.Image")));
+            this.nuevoToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nuevoToolStripMenuItem.Text = "&Nuevo";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "&Salir";
+            // 
+            // acercadeToolStripMenuItem
+            // 
+            this.acercadeToolStripMenuItem.Name = "acercadeToolStripMenuItem";
+            this.acercadeToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.acercadeToolStripMenuItem.Text = "&Acerca de...";
+            this.acercadeToolStripMenuItem.Click += new System.EventHandler(this.acercadeToolStripMenuItem_Click);
+            // 
+            // rtboxMensajesRecibidos
+            // 
+            this.rtboxMensajesRecibidos.Enabled = false;
+            this.rtboxMensajesRecibidos.Location = new System.Drawing.Point(12, 51);
+            this.rtboxMensajesRecibidos.Name = "rtboxMensajesRecibidos";
+            this.rtboxMensajesRecibidos.Size = new System.Drawing.Size(422, 262);
+            this.rtboxMensajesRecibidos.TabIndex = 2;
+            this.rtboxMensajesRecibidos.Text = "";
+            // 
+            // tboxMensajeAEnviar
+            // 
+            this.tboxMensajeAEnviar.Location = new System.Drawing.Point(15, 337);
+            this.tboxMensajeAEnviar.Name = "tboxMensajeAEnviar";
+            this.tboxMensajeAEnviar.Size = new System.Drawing.Size(325, 20);
+            this.tboxMensajeAEnviar.TabIndex = 3;
+            // 
+            // btnEnviarFichero
+            // 
+            this.btnEnviarFichero.Location = new System.Drawing.Point(346, 374);
+            this.btnEnviarFichero.Name = "btnEnviarFichero";
+            this.btnEnviarFichero.Size = new System.Drawing.Size(88, 23);
+            this.btnEnviarFichero.TabIndex = 5;
+            this.btnEnviarFichero.Text = "Enviar fichero";
+            this.btnEnviarFichero.UseVisualStyleBackColor = true;
+            this.btnEnviarFichero.Click += new System.EventHandler(this.btnEnviarFichero_Click);
+            // 
+            // lblMensajesRecibidos
+            // 
+            this.lblMensajesRecibidos.AutoSize = true;
+            this.lblMensajesRecibidos.Location = new System.Drawing.Point(12, 35);
+            this.lblMensajesRecibidos.Name = "lblMensajesRecibidos";
+            this.lblMensajesRecibidos.Size = new System.Drawing.Size(97, 13);
+            this.lblMensajesRecibidos.TabIndex = 6;
+            this.lblMensajesRecibidos.Text = "Mensajes recibidos";
+            // 
+            // btnEnviarMensaje
+            // 
+            this.btnEnviarMensaje.Location = new System.Drawing.Point(346, 337);
+            this.btnEnviarMensaje.Name = "btnEnviarMensaje";
+            this.btnEnviarMensaje.Size = new System.Drawing.Size(88, 23);
+            this.btnEnviarMensaje.TabIndex = 7;
+            this.btnEnviarMensaje.Text = "Enviar mensaje";
+            this.btnEnviarMensaje.UseVisualStyleBackColor = true;
+            this.btnEnviarMensaje.Click += new System.EventHandler(this.btnEnviarMensaje_Click);
+            // 
+            // FormChat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 409);
+            this.Controls.Add(this.btnEnviarMensaje);
+            this.Controls.Add(this.lblMensajesRecibidos);
+            this.Controls.Add(this.btnEnviarFichero);
+            this.Controls.Add(this.tboxMensajeAEnviar);
+            this.Controls.Add(this.rtboxMensajesRecibidos);
+            this.Controls.Add(this.menuPrincipal);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuPrincipal;
+            this.Name = "FormChat";
+            this.Text = "Hiperterminal";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormChat_FormClosed);
+            this.menuPrincipal.ResumeLayout(false);
+            this.menuPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercadeToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox rtboxMensajesRecibidos;
+        private System.Windows.Forms.TextBox tboxMensajeAEnviar;
+        private System.Windows.Forms.Button btnEnviarFichero;
+        private System.Windows.Forms.Label lblMensajesRecibidos;
+        private System.Windows.Forms.Button btnEnviarMensaje;
+    }
+}
---
+++ Practica10/Practica10/FormChat.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica10
+{
+    public partial class FormChat : Form
+    {
+        private SerialPort puertoSerie;
+        public FormChat(SerialPort puertoSerie)
+        {
+            InitializeComponent();
+            CenterToScreen();
+            this.puertoSerie = puertoSerie;
+            this.puertoSerie.DataReceived += new SerialDataReceivedEventHandler(this.recibirMensajes);
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void FormChat_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void acercadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Programa realizado por Pablo Navarro Vázquez" + "\nVersión: 1.0.0","Acerca de",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void recibirMensajes(object sender,EventArgs e)
+        {
+            rtboxMensajesRecibidos.Text += puertoSerie.ReadLine();
+        }
+
+        private void btnEnviarFichero_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnEnviarMensaje_Click(object sender, EventArgs e)
+        {
+            puertoSerie.WriteLine(tboxMensajeAEnviar.Text);
+        }
+    }
+}
